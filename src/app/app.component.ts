ff4e399aeb36acd5a9ded2451021bcefdffeabb6
Pasupathi P <pasupathi.p@optisolbusiness.com>
2023-03-02 16:39:03 +0530

    Add existing project files to Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocketService } from './socket.service';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ecommerce';
+  private hubConnectionBuilder!: HubConnection;
+  offers: any[] = [];
+  title = 'Chatbot';
+  messageArray = [];
+  synth:any;
+  voices:any;
+  constructor(private socketService:SocketService) {
+    this.synth = window.speechSynthesis;
+    this.voices = this.synth.getVoices();
+  }
+  message= '';
+
+  ngOnInit(){
+    this.socketService.receivedReply().subscribe(data=> {
+      this.messageArray.push({name:'bot', message: data.outputMessage});
+      this.speak(data.outputMessage);
+    });
+
+    this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('https://localhost:7219/offers').configureLogging(LogLevel.Information).build();
+        this.hubConnectionBuilder.start().then(() => console.log('Connection started.......!')).catch(err => console.log('Error while connect with server'));
+        this.hubConnectionBuilder.on('SendOffersToUser', (result: any) => {
+            this.offers.push(result);
+        });
+
+  }
+
+  sendMessage(){
+    if(this.message) {
+      const data = { message:this.message };
+      this.socketService.sendMessage(data);
+      this.messageArray.push({name:'you', message:this.message});
+      this.message = '';
+    }
+  }
+
+ speak(string) {
+  let u = new SpeechSynthesisUtterance(string);
+  u.text = string;
+  u.lang = "en-US";
+  u.volume = 1; //0-1 interval
+  u.rate = 1;
+  u.pitch = 1; //0-2 interval
+  this.synth.speak(u);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocketService } from './socket.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ChatComponent } from './chat/chat.component';
+import { AppRoutingModule } from './app.routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { ChatService } from './chat.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+const routes: Routes = [
+  { path: "chat", component: ChatComponent }
+//   { path: "contactus", component: ContactUsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [SocketService, ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
